41ecd0db5f05eb755be0f2acd43fedb1739113b1
Budi Gunawan <budiindra.g@gmail.com>
2024-02-29 17:22:34 +0700

    feat: add styling to navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ['./*.{html,js}'],
   theme: {
-    extend: {},
+    colors: {
+      primary: '#3B82F6',
+      'primary-light': '#C6DBFF',
+      dark: '#193766',
+      muted: '#697B98',
+      'muted-light': '#FFFFFF',
+    },
+    fontFamily: {
+      sans: ['DM Sans', 'sans-serif'],
+    },
+    extend: {
+      maxWidth: {
+        295: '73.75rem',
+      },
+    },
   },
   plugins: [],
 };
